7f604f08148c0a84b87b17d5a257dbcd5339527e
Evy <evy.heynen@telenet.be>
2017-11-20 14:11:19 +0100

    Question.class

--- src/main/java/be/oak3/evaluation/model/Question.java
+++ src/main/java/be/oak3/evaluation/model/Question.java
@@ -1,4 +1,39 @@
 package be.oak3.evaluation.model;
 
 public class Question {
+
+    private int id;
+    private String question;
+
+    public Question() {
+    }
+
+    public Question(int id, String question) {
+        this.id = id;
+        this.question = question;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", question='" + question + '\'' +
+                '}';
+    }
 }
