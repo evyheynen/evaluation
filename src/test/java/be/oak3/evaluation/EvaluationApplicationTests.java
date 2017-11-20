c33fc466d1970404b9ef8bea744396036ba08768
Evy <evy.heynen@telenet.be>
2017-11-20 12:05:26 +0100

    Initial commit

---
+++ src/test/java/be/oak3/evaluation/EvaluationApplicationTests.java
@@ -0,0 +1,16 @@
+package be.oak3.evaluation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EvaluationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
