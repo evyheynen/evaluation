184c559bf07adea44e24961cf78bd4f8eb449ddc
Evy <evy.heynen@telenet.be>
2017-11-20 14:40:30 +0100

    Question class @Entity

--- src/main/java/be/oak3/evaluation/model/Question.java
+++ src/main/java/be/oak3/evaluation/model/Question.java
@@ -1,8 +1,18 @@
 package be.oak3.evaluation.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Question {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    @NotNull
     private String question;
 
     public Question() {
