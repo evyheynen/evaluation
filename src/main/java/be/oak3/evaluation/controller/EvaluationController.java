508b78c6c3f2e84c01fd78ff30a78d5ae0b2fbdd
Evy <evy.heynen@telenet.be>
2017-11-20 15:36:06 +0100

    Controller

--- src/main/java/be/oak3/evaluation/controller/EvaluationController.java
+++ src/main/java/be/oak3/evaluation/controller/EvaluationController.java
@@ -1,4 +1,26 @@
 package be.oak3.evaluation.controller;
 
+import be.oak3.evaluation.model.Evaluation;
+import be.oak3.evaluation.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/evaluation")
 public class EvaluationController {
+
+    private EvaluationService service;
+
+    @Autowired
+    public EvaluationController(EvaluationService service){
+        this.service = service;
+    }
+
+    @RequestMapping (method= RequestMethod.POST)
+    public ResponseEntity addEvaluation(Evaluation evaluation){
+        return ResponseEntity.ok(service.addEvaluation(evaluation));
+    }
 }
